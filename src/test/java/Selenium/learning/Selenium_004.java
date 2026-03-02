fbd5eae4b4c644ebf5c4b62ccd7e18d47ac9501d
Sowjanya <sowjanyajanya98@ggmail.com>
2026-03-02 15:37:34 +0530

    Locators

--- src/test/java/Selenium/learning/Selenium_004.java
+++ src/test/java/Selenium/learning/Selenium_004.java
@@ -20,15 +20,28 @@ public class Selenium_004 {
         driver.findElement(By.linkText("Blog")).click();
         Thread.sleep(5000);
 
-       /* usig partialLinktext (not recommended suppose if we hve 2 links with same text
+       /*
+       usig partialLinktext (not recommended suppose if we hve 2 links with same text
         "Smart add" is the linktext but ive given only the partial text
-        i.e Smart still it working.?*/
+        i.e Smart still it working.?
+        */
 
         driver.findElement(By.partialLinkText("Smart")).click();
-        // Listings all tag names <a>
+
+
+        /*
+        Listings all tag names <a>, all the links will be listed, which are available in
+        webpage.
+        */
+
         List<WebElement> links = driver.findElements(By.tagName("a"));
         System.out.println("total number of links:" + links.size());
         Thread.sleep(5000);
+
+        //listing all the images from the webside
+
+        List<WebElement> image = driver.findElements(By.tagName("img"));
+        System.out.println("Total number of image:" + image.size());
 
         driver.quit();
 
